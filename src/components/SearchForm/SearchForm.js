b262400a3b008e24db74468be28df7b584d9ae91
Stephanie Guzman <sguzman247@gmail.com>
2022-12-20 15:41:20 -0600

    Add search form and event handlers

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,42 @@
-import { useQuery, gql } from '@apollo/client'
+import { useLazyQuery, gql } from '@apollo/client'
+import { useState } from 'react'
+import { GET_SEARCH_RESULTS } from '../../queries'
 
 const SearchForm = () => {
+  const [ searchInput, setSearchInput ] = useState('')
+
+  const [ getSearchResults, {loading, data, error, refetch }] = useLazyQuery(GET_SEARCH_RESULTS)
+
+  const handleSearch = (event) => {
+    // getSearchResults({
+    //   variables: { title: searchInput.toUpperCase()}
+    // })
+    event.preventDefault()
+    console.log('I made it in!')
+  }
+
+  if (loading) return <p className="loading-message">Loading...</p>
+  // if (error || !data) return <p className='error-message'>Oops! No results match your search terms. Try your search again.</p>
+  if (data) {
+    console.log(data)
+  }
+
   return (
     <div className='search-form-container'>
-      <h3>this is the search form</h3>
+      <form className='search-form' data-cy='search-form'>
+        <input 
+          type='text'
+          name='search'
+          placeholder='Search'
+          data-cy='search-form-input'
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
+        />
+        <button 
+          className='search-form-button' 
+          data-cy='search-form-button' 
+          onClick={(event) => handleSearch(event)}>SUBMIT</button>
+      </form>
     </div>
   )
 }

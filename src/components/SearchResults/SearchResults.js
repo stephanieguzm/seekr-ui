5578e2c7043444b7fcc0b8b82154e4dfc63900eb
Stephanie Guzman <sguzman247@gmail.com>
2022-12-20 17:08:14 -0600

    Pass down props to Result component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,10 +1,26 @@
 import Result from '../Result/Result'
 
 const SearchResults = ({ data }) => {
+  const companies = data.keywordSearch
+
+  const companyListings = companies.map(company => {
+    return <Result
+      companyName={company.companyName}
+      description={company.description}
+      hiringLink={company.hiringLink}
+      locationsHiringIn={company.locationsHiringIn}
+      oneNiceThing={company.oneNiceThing}
+      rolesHiringFor={company.rolesHiringFor}a
+      id={company.id}
+      key={company.id}
+    />
+  }
+  )
+
   return (
     <div className='search-results-container'>
-      <h3>{data.companyName}</h3>
-      <Result />
+      <h3>These are your search results!</h3>
+      {companyListings}
     </div>
   )
 }

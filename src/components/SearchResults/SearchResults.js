87805e0491fc2e664892e1d7bdf395f83476721e
Stephanie Guzman <sguzman247@gmail.com>
2023-01-03 18:06:48 -0600

    Make ExpandedResult a child of SearchResults component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,12 +1,17 @@
 import Result from '../Result/Result'
+import ExpandedResult from '../ExpandedResult/ExpandedResult'
 import './SearchResults.css'
+import { useState } from 'react'
 
 const SearchResults = ({ data }) => {
+
+  const [ selectedCompany, setSelectedCompany ] = useState({})
   const companies = data.keywordSearch
 
   const companyListings = companies.map(company => {
     return <Result
       company={company}
+      setSelectedCompany={setSelectedCompany}
       id={company.id}
       key={company.id}
     />
@@ -15,6 +20,10 @@ const SearchResults = ({ data }) => {
   return (
     <div className='search-results-container'>
       {companyListings}
+      {selectedCompany && <ExpandedResult 
+        selectedCompany={selectedCompany}
+        key={selectedCompany.id}
+      />}
     </div>
   )
 }

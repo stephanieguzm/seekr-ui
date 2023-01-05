efec56e7d421a325066118bb193f4abf98ab6598
Stephanie Guzman <sguzman247@gmail.com>
2023-01-04 20:55:36 -0600

    Add styling + render expanded result data

--- src/components/ExpandedResult/ExpandedResult.js
+++ src/components/ExpandedResult/ExpandedResult.js
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom'
 import './ExpandedResult.css'
 
 const ExpandedResult = ({ selectedCompany }) => {
+  console.log(selectedCompany)
   return (
-    <div className='expanded-result-container'>
-      <h3>{selectedCompany.companyName}</h3>
-      <h4>{selectedCompany.locationsHiringIn}</h4>
-      <h4>{selectedCompany.rolesHiringFor}</h4>
+    <div className='expanded-result-container' id={selectedCompany.id}>
+      <div className='exp-result-header'>
+        <h3>{selectedCompany.companyName}</h3>
+        <Link path={selectedCompany.hiringLink}>
+          <button className='apply-button'>APPLY HERE</button>
+        </Link>
+      </div>
+      <p className=''>Location(s): {selectedCompany.locationsHiringIn}</p>
+      <p>Hiring for: {selectedCompany.rolesHiringFor}</p>
+      {selectedCompany.description && <p>Description: {selectedCompany.description}</p>}
+      {selectedCompany.oneNiceThing && <p>One nice thing: {selectedCompany.oneNiceThing}</p>}
+      {selectedCompany.comments && <p>Comments: {selectedCompany.comments}</p>}
     </div>
   )
 }
